2d2e19a129ad2a3a653ffc9f30d5202e5221491a
Josiah King <josiahaccounts@gmail.com>
2020-04-08 12:02:17 +0100

    Added database config and modified License type

--- configs/user.config.php
+++ configs/user.config.php
@@ -4,9 +4,14 @@
  * User custom config
  * Define all your global variable/const here
  */
+//DO NOT REMOVE THE BELOW CONFIGS
 define('DEBUG', TRUE);
 define('BASE_ERROR_PAGE_404',BASE_URL."error/404");
 define('DEFAULT_ROUTE',['EvolvePhpComponent\site\controllers\SiteController','home']);
+define('SESSION_NAME',"EvolvePhp");//set session name
+define('SESSION_EXPIRE',120);//in minutes
+define('SESSION_SAVE_PATH',"");//default path is used is not set
+//ADD YOURS BELOW THIS COMMENT
 define('SITE_NAME', '');
 define('SITE_SHORT_NAME', '');
 define('SITE_EMAIL', '');
